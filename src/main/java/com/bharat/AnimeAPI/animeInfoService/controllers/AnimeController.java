8ea510b7b927f8c80b12b5104e4f21dbaa57d4c4
xZCute OP <bharatmhes@gmail.com>
2023-07-29 11:00:01 +0530

    Configured gogoanime api service into the backend

---
+++ src/main/java/com/bharat/AnimeAPI/animeInfoService/controllers/AnimeController.java
@@ -0,0 +1,33 @@
+package com.bharat.AnimeAPI.animeInfoService.controllers;
+
+import com.bharat.AnimeAPI.animeInfoService.models.*;
+import com.bharat.AnimeAPI.animeInfoService.services.AnimeInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/anime")
+public class AnimeController {
+    @Autowired
+    private AnimeInfoService animeInfoService;
+
+    @GetMapping("/search/{name}")
+    public AnimeSearch searchAnime(@PathVariable String name, @RequestParam(defaultValue = "1") String page){
+        return animeInfoService.searchAnime(name, page);
+    }
+
+    @GetMapping("/details/{animeId}")
+    public AnimeDetails getDetails(@PathVariable String animeId){
+        return animeInfoService.getDetails(animeId);
+    }
+
+    @GetMapping("/top-airing")
+    public AnimeTopAiringSearch getTopAiringAnimes(@RequestParam(defaultValue = "1") String page){
+        return animeInfoService.getTopAiring(page);
+    }
+
+    @GetMapping("/genre/{genre}")
+    public AnimeGenresSearch getGenresAnime(@PathVariable String genre, @RequestParam(defaultValue = "1") String page){
+        return animeInfoService.getGenreAnime(genre, page);
+    }
+}
---
+++ src/main/java/com/bharat/AnimeAPI/animeInfoService/services/AnimeInfoService.java
@@ -0,0 +1,90 @@
+package com.bharat.AnimeAPI.animeInfoService.services;
+
+import com.bharat.AnimeAPI.animeInfoService.models.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class AnimeInfoService {
+    @Value("${animeapi.uri}")
+    private String animeApiUri;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @CircuitBreaker(name = "search-cb", fallbackMethod = "searchFallback")
+    public AnimeSearch searchAnime(String name, String page){
+        ResponseEntity<List<Anime>> searchResponse =
+                restTemplate.exchange(animeApiUri + "search?keyw=" + name + "&page=" + page,
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>(){
+                        });
+
+        return AnimeSearch.builder().animes(searchResponse.getBody()).build();
+    }
+
+    @CircuitBreaker(name = "details-cb", fallbackMethod = "detailsFallBack")
+    public AnimeDetails getDetails(String animeId){
+        return restTemplate.getForObject(
+            animeApiUri + "anime-details/" + animeId,
+                AnimeDetails.class
+        );
+    }
+
+    @CircuitBreaker(name = "topAiring-cb", fallbackMethod = "topAiringFallback")
+    public AnimeTopAiringSearch getTopAiring(String page){
+
+        ResponseEntity<List<AnimeTopAiring>> topAiringAnimes =
+                restTemplate.exchange(
+                        animeApiUri + "/top-airing?page=" + page,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<AnimeTopAiring>>(){}
+                );
+        return AnimeTopAiringSearch.builder().animes(topAiringAnimes.getBody()).build();
+    }
+
+    @CircuitBreaker(name = "genre-cb", fallbackMethod = "genresSearchFallBack")
+    public AnimeGenresSearch getGenreAnime(String genre, String page){
+        ResponseEntity<List<AnimeGenres>> genresResponse =
+                restTemplate.exchange(
+                        animeApiUri + "/genre/" + genre + "?page=" + page,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<AnimeGenres>>(){}
+                );
+
+        return AnimeGenresSearch.builder().animes(
+                genresResponse.getBody()
+        ).build();
+    }
+
+    public AnimeSearch searchFallback(String name, Exception e){
+        return AnimeSearch.builder().animes(List.of(
+                Anime.builder().animeTitle("Could not found").build()
+        )).build();
+    }
+
+    public AnimeDetails detailsFallBack(String animeId, Exception e){
+        return AnimeDetails.builder().animeTitle("Could not found this anime details").build();
+    }
+
+    public AnimeTopAiringSearch topAiringFallback(Exception e){
+        return AnimeTopAiringSearch.builder().animes(
+                List.of(AnimeTopAiring.builder().animeTitle("Could not find any animes").build())
+        ).build();
+    }
+
+    public AnimeGenresSearch genresSearchFallBack(String genres, Exception e){
+        return AnimeGenresSearch.builder().animes(
+                List.of(AnimeGenres.builder().animeTitle("Could not find any anime of this genre").build())
+        ).build();
+    }
+}
